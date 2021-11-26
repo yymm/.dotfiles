8a3ab3b797d3f24f39f888f20f4152e563f58080
yuya yano <yuya.yano.6260@gmail.com>
2021-11-27 02:10:48 +0900

    update on ubuntu

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -68,3 +68,5 @@ if type -q java
 end
 
 alias dco="docker-compose"
+
+set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
